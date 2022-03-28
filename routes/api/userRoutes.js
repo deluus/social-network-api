c42267b8ef9315fccb030c7ac2fe772825231be6
deluus polycarp <pdeluus@gmail.com>
2022-03-28 11:53:11 -0700

    created routes in index and user routes. connecting to test

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,14 +3,25 @@ const router = require('express').Router();
 const {
     createUser,
     getAllUsers,
-    getUserById
+    getUserById,
+    addFriend,
+    updateUser,
+    deleteUser,
+    removeFriend
 } = require('../../controllers/user-controller');
 
 router.route('/')
     .post(createUser)
-    .get(getAllUsers);
+    .get(getAllUsers)
+    .post(createUser);
 
-router.route('/:userId')
+router.route('/:Id')
     .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+ router.route('/:userId/friends/:friendId')   
+    .post(addFriend)
+    .delete(removeFriend)
 
     module.exports = router;
